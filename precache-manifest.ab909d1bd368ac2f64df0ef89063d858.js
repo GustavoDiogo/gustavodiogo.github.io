e8c53e087586ec4853581f342517ce803495a749
Gustavo <gustavo.silva@drtis.com.br>
2020-08-20 19:23:40 -0300

    Updates

---
+++ precache-manifest.ab909d1bd368ac2f64df0ef89063d858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16671698b5068c46bc645e3509f92ae6",
+    "url": "/gustavodiogo.github.io/index.html"
+  },
+  {
+    "revision": "b4e72128578c8e7caaf5",
+    "url": "/gustavodiogo.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de138d1a7aaa642c70c5",
+    "url": "/gustavodiogo.github.io/static/js/2.d0a0f88e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gustavodiogo.github.io/static/js/2.d0a0f88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e72128578c8e7caaf5",
+    "url": "/gustavodiogo.github.io/static/js/main.fd7b112e.chunk.js"
+  },
+  {
+    "revision": "186c930766d10b049b93",
+    "url": "/gustavodiogo.github.io/static/js/runtime-main.826e5684.js"
+  },
+  {
+    "revision": "113e8f159838e91b280e0b85b76465a9",
+    "url": "/gustavodiogo.github.io/static/media/fotoQuemSouEu.113e8f15.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8de4e49c847a5e82ff22f26100211863.js"
+  "/gustavodiogo.github.io/precache-manifest.ab909d1bd368ac2f64df0ef89063d858.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gustavodiogo.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
